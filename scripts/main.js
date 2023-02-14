a59d64a6c95cedb5c625cc0cfe1948930b26064c
Lauren Hooton <lkhooton@gmail.com>
2023-02-14 11:07:07 -0500

    add gif

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
+  document.getElementById('gif').src='https://media.tenor.com/FTMkBM_rbx0AAAAC/moon.gif';
 }
