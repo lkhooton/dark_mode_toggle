2c26b420e2fd61d1ae276533781c6aa6058c4db0
Lauren Hooton <lkhooton@gmail.com>
2023-02-14 13:16:00 -0500

    done

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,18 @@
-function myFunction() {
+function darkMode () {
   var element = document.body;
   element.classList.toggle("dark-mode");
-  document.getElementById('gif').src='https://media.tenor.com/FTMkBM_rbx0AAAAC/moon.gif';
+}
+
+let image_tracker = 'bright';
+
+function change(){
+  
+  let image = document.getElementById('gif');
+  if(image_tracker=='bright'){
+    image.src='https://media.tenor.com/FTMkBM_rbx0AAAAC/moon.gif';
+    image_tracker='dark';
+  }else{
+    image.src="https://media.tenor.com/kTj5MU39mmwAAAAC/morning-sunshine.gif" ;
+    image_tracker='bright';
+  }
 }
